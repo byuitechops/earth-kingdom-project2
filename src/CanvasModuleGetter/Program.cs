8afcd02273bcd8da94b674e9072bd73180ecbe3a
KayleeHartzog <har15031@byui.edu>
2019-06-24 15:08:58 -0600

    worked on the transform and program

--- src/CanvasModuleGetter/Program.cs
+++ src/CanvasModuleGetter/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
 
 namespace CanvasModuleGetter
 {
@@ -16,8 +18,36 @@ namespace CanvasModuleGetter
             string api_token = System.Environment.GetEnvironmentVariable("CANVAS_API_TOKEN");
             string endPoint = args.Length != 0 ? args[0] : "/api/v1/courses/" + courseId;
             string result = await ApiCall.MakeHTTPRequest(endPoint, api_token);
-            dynamic json = JsonConvert.DeserializeObject(result);
-            Console.WriteLine(json);
+            dynamic data = JsonConvert.DeserializeObject(result);
+            //Console.WriteLine(data);
+
+            switch(reportType)
+            {
+                case "CSV":
+                    List<object> json = new List<dynamic>();
+                    if (data is JArray)
+                    {
+                        foreach (var prop in data)
+                        {
+                            json.Add(prop);
+                        }
+                    }
+                    else
+                    {
+                         json.Add(data);
+                    }
+                    CsvTransform.CsvTransformer(json);
+                    break;
+                case "JSON":
+                    Console.WriteLine("this is json");
+                    break;
+                case "HTML":
+                    Console.WriteLine("this is html");
+                    break;
+                default:
+                    Console.WriteLine("Not valid file type");
+                    break;
+            }
         }
     }
 }
--- src/CanvasModuleGetter/Transform.cs
+++ src/CanvasModuleGetter/Transform.cs
@@ -1,4 +1,10 @@
 using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using CsvHelper;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json.Linq;
 
 namespace CanvasModuleGetter
 {
@@ -8,9 +14,35 @@ namespace CanvasModuleGetter
 
     }
 
-    class CsvTransform : Transform
+    public class CsvTransform : Transform
     {
+        public static void CsvTransformer(List <dynamic> json) {
+            System.IO.File.WriteAllText(Directory.GetCurrentDirectory() + @"\activity02.csv",string.Empty);
 
+
+            using (TextWriter writer = new StreamWriter(Directory.GetCurrentDirectory() + @"\activity02.csv", true, System.Text.Encoding.UTF8))
+            {
+                var csv = new CsvWriter(writer);
+                var firstObject = json[0];
+
+                foreach (JProperty property in firstObject)
+                    csv.WriteField(property.Name);
+                csv.NextRecord();
+
+
+
+                foreach (var obj in json)
+                {
+                    foreach (JProperty prop in obj)
+                    {
+                        csv.WriteField(prop.Value.ToString());
+                    }
+                    csv.NextRecord();
+                }
+
+                writer.Flush();
+            }
+        }
     }
 
     class JsonTransform : Transform
