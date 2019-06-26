9319b105d7e04916ba47e7aa4e8994f4d02377d9
evgi88 <bek17003@byui.edu>
2019-06-25 18:55:44 -0600

    Added a constructors test

--- test/CanvasModuleGetter.Tests/UserInputTests.cs
+++ test/CanvasModuleGetter.Tests/UserInputTests.cs
@@ -7,9 +7,35 @@ namespace CanvasModuleGetter.Tests
     public class userInputTest
     {
         [Fact]
+        public void constructorsTest()
+        {
+            //test default constructor
+            UserInput userInput = new UserInput();
+
+            Assert.Equal("", userInput.getReportType());
+            Assert.Equal("", userInput.getToken());
+            Assert.Equal(0, userInput.getCourseIds().Count);
+
+            //test non-default constructor
+            List<string> courses = new List<string>()
+            {
+                "10", "20"
+            };
+            string reportType = "JSON";
+            UserInput userInput1 = new UserInput(courses, reportType);
+
+            Assert.Equal("JSON", userInput1.getReportType());
+            Assert.Equal(System.Environment.GetEnvironmentVariable("CANVAS_API_TOKEN"), userInput1.getToken());
+            Assert.Equal(2, userInput1.getCourseIds().Count);
+            Assert.Equal("10", userInput1.getCourseIds()[0]);
+            Assert.Equal("20", userInput1.getCourseIds()[1]);
+        }
+
+        [Fact]
         public void tokenTest()
         {
-
+            UserInput userInput = new UserInput();
+            Assert.Equal("", userInput.getToken());
         }
 
         [Fact]
@@ -17,17 +43,17 @@ namespace CanvasModuleGetter.Tests
         {
             List<string> courses = new List<string>()
             {
-                "80", "80"
+                "80", "90"
             };
             string reportType = "JSON";
 
             UserInput userInput = new UserInput(courses, reportType);
 
             Assert.Equal("80", userInput.getCourseIds()[0]);
-            Assert.Equal("80", userInput.getCourseIds()[1]);
+            Assert.Equal("90", userInput.getCourseIds()[1]);
         }
 
-        //[Fact]
+        [Fact]
         public void reportTypeTest()
         {
             List<string> courses = new List<string>()
